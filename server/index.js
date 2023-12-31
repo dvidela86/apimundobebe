d8ad6eed28eaf13807de36626c379863220c6fa0
dvidela86 <143230554+dvidela86@users.noreply.github.com>
2023-12-31 13:40:32 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
-import express from "express";
-import cors from "cors";
-import PORT from './config.js';
-import indexRoutes from './routes/index.routes.js';
-import taskRoutes from './routes/task.routers.js';
-
+const express = require("express");
+const cors = require("cors");
+const PORT = require('./config.js');
+const indexRoutes = require('./routes/index.routes.js');
+const taskRoutes = require('./routes/task.routers.js');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(indexRoutes)
-app.use(taskRoutes)
-app.listen(PORT)
-console.log('server se ejecuta en el puerto 3000')
+app.use(indexRoutes);
+app.use(taskRoutes);
+app.listen(PORT);
+console.log('server se ejecuta en el puerto 3000');
